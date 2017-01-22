2fe798726b57da6ea4a110e943fed21b28d1436c
Oclemy <oclemmi@gmail.com>
2017-01-22 01:03:15 -0800

    Initial Commit

---
+++ app/src/main/java/com/tutorials/hp/sqliterushorm/mListView/MyLongClickListener.java
@@ -0,0 +1,10 @@
+package com.tutorials.hp.sqliterushorm.mListView;
+
+/**
+ * Created by Oclemmy on 5/5/2016 for ProgrammingWizards Channel and http://www.Camposha.com.
+ */
+public interface MyLongClickListener {
+
+    void onItemLongClick();
+
+}
